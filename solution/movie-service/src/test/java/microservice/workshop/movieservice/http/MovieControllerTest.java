475dd8e944e48dcb4147ef1f04cb2eceafebb140
Jeff Butler <jeffgbutler@gmail.com>
2020-11-02 18:08:03 -0500

    Update Movie Service Versions

--- solution/movie-service/src/test/java/microservice/workshop/movieservice/http/MovieControllerTest.java
+++ solution/movie-service/src/test/java/microservice/workshop/movieservice/http/MovieControllerTest.java
@@ -7,18 +7,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class MovieControllerTest {
 
@@ -27,7 +27,7 @@ public class MovieControllerTest {
     @Autowired
     private WebApplicationContext webApplicationContext;
     
-    @Before
+    @BeforeEach
     public void setup() {
         this.mockMvc = webAppContextSetup(webApplicationContext).build();
     }
@@ -36,7 +36,7 @@ public class MovieControllerTest {
     public void testExists() throws Exception {
         mockMvc.perform(get("/movie/1"))
         .andExpect(status().is(HttpStatus.OK.value()))
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
         .andExpect(jsonPath("$.title", is("The Godfather")));
     }
 
