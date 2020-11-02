6b5ca901b27ae8a2638cd590863debd5ec43b3d8
Jeff Butler <jeffgbutler@gmail.com>
2020-11-02 18:28:47 -0500

    Update Movie Cast Service Versions

--- solution/movie-cast-service/src/test/java/microservice/workshop/moviecastservice/MovieCastServiceApplicationTests.java
+++ solution/movie-cast-service/src/test/java/microservice/workshop/moviecastservice/MovieCastServiceApplicationTests.java
@@ -1,15 +1,12 @@
 package microservice.workshop.moviecastservice;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.TestFactory;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class MovieCastServiceApplicationTests {
 
-    @Test
+    @TestFactory
     public void contextLoads() {
     }
 
