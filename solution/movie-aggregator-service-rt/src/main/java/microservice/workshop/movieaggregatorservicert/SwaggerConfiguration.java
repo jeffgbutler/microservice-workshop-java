419206a217ba8796d4c51729bf2f71cd2c2d7aa7
Jeff Butler <jeffgbutler@gmail.com>
2021-11-15 13:28:32 -0500

    Migrate from SpringFox to SpringDoc

--- solution/movie-aggregator-service-rt/src/main/java/microservice/workshop/movieaggregatorservicert/SwaggerConfiguration.java
+++ solution/movie-aggregator-service-rt/src/main/java/microservice/workshop/movieaggregatorservicert/SwaggerConfiguration.java
@@ -1,23 +1,10 @@
 package microservice.workshop.movieaggregatorservicert;
 
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
-
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
+@OpenAPIDefinition(info = @Info(title = "Movie Aggregator Service API (REST Template)", version = "v0.0.1"))
 public class SwaggerConfiguration {
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
-                .build();
-    }
 }
