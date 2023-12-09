d339f61a5bfe9f215d3e647d99b79affc29e49db
Jeff Butler <jeffgbutler@gmail.com>
2023-12-09 15:52:47 -0500

    Add and Configure a Second Eureka Server

---
+++ solution/eureka-server-2/src/test/java/microservice/workshop/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,12 @@
+package microservice.workshop.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EurekaServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
