e275b5aafd5e1b13173d82fae11613236fd34de0
Jeff Butler <jeffgbutler@gmail.com>
2024-02-28 14:27:07 -0500

    Remove unused import

--- exercises/movie-award-service/src/main/java/microservice/workshop/movieawardservice/http/AwardController.java
+++ exercises/movie-award-service/src/main/java/microservice/workshop/movieawardservice/http/AwardController.java
@@ -2,7 +2,6 @@ package microservice.workshop.movieawardservice.http;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
